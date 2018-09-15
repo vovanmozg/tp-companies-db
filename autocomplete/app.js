1c23f4c7c76674ce11d6ca1450d5bdce7469c1dd
Vladimir Polukhin <vovanmozg@gmail.com>
2018-09-15 07:26:00 +0300

    Move index page

--- autocomplete/app.js
+++ autocomplete/app.js
@@ -2,14 +2,20 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const db = require('./infrastructure/db');
+
+const indexRouter = require('./routes/index');
 const autocompleteRouter = require('./routes/autocomplete');
 
 const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', indexRouter);
 app.use('/autocomplete', autocompleteRouter);
 
 // catch 404 and forward to error handler
